ac2118cfe7bd0e737e833ef378873805a6f25c6e
Derek <derekdanielson84@gmail.com>
2023-06-19 11:00:25 -0600

    added formbtn to global scope

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
 const clearButton = document.getElementById('clear');
 const itemFilter = document.getElementById('filter');
+const formBtn = itemForm.querySelector('button');
 let isEditMode = false;
 
 
